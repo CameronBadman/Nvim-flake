a3a62a22db09befc921b360af43d4ddf1067549f
CameronBadman <cbadwork@gmail.com>
2025-08-08 15:46:46 +1000

    formatted some files

--- flake.nix
+++ flake.nix
@@ -8,40 +8,60 @@
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { nixpkgs, nixvim, flake-parts, ... }@inputs:
+  outputs =
+    {
+      nixpkgs,
+      nixvim,
+      flake-parts,
+      ...
+    }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      
-      perSystem = { config, self', inputs', pkgs, system, ... }: 
-      let
-        # Configure nixpkgs to allow unfree packages
-        pkgs-unfree = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          # Configure nixpkgs to allow unfree packages
+          pkgs-unfree = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+            };
           };
-        };
-      in {
-        packages = {
-          default = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-            pkgs = pkgs-unfree;
-            module = [
-              ./config
-              ./languages.nix
+        in
+        {
+          packages = {
+            default = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+              pkgs = pkgs-unfree;
+              module = [
+                ./config
+                ./languages.nix
+              ];
+            };
+          };
+
+          devShells.default = pkgs-unfree.mkShell {
+            buildInputs = with pkgs-unfree; [
+              nixvim.packages.${system}.default
             ];
           };
         };
-        
-        devShells.default = pkgs-unfree.mkShell {
-          buildInputs = with pkgs-unfree; [
-            nixvim.packages.${system}.default
-          ];
-        };
-      };
-      
+
       flake = {
         nixvimModule = ./config;
-        
+
         # Export the extraPackages directly
         extraPackages = pkgs: (import ./languages.nix { inherit pkgs; }).extraPackages;
       };
--- languages.nix
+++ languages.nix
@@ -4,57 +4,111 @@
   # Include language toolchains (compilers/runtimes) that LSPs need to function
   extraPackages = with pkgs; [
     # C/C++ Compilers & Build Tools (for clangd LSP)
-    gcc clang cmake gnumake pkg-config gdb lldb valgrind
+    gcc
+    clang
+    cmake
+    gnumake
+    pkg-config
+    gdb
+    lldb
+    valgrind
     # Rust Toolchain (for rust_analyzer LSP)
-    rustc cargo rustfmt clippy
+    rustc
+    cargo
+    rustfmt
+    clippy
     # Python Runtime (for pyright LSP)
-    python311 python311Packages.pip python311Packages.setuptools python311Packages.wheel poetry black isort mypy ruff
+    python311
+    python311Packages.pip
+    python311Packages.setuptools
+    python311Packages.wheel
+    poetry
+    black
+    isort
+    mypy
+    ruff
     # Go Compiler (for gopls LSP)
-    go gofumpt golangci-lint delve
+    go
+    gofumpt
+    golangci-lint
+    delve
     # Java Runtime (for jdtls LSP)
-    jdk21 maven gradle
+    jdk21
+    maven
+    gradle
     # C# Runtime (for omnisharp LSP)
-    dotnet-sdk_8 mono
+    dotnet-sdk_8
+    mono
     # Gleam Runtime (for gleam LSP)
-    gleam erlang
+    gleam
+    erlang
     # OCaml Runtime (for ocamllsp LSP)
-    ocaml dune_3 opam ocamlformat
-    
+    ocaml
+    dune_3
+    opam
+    ocamlformat
+
     # JavaScript/TypeScript Runtime (for tsserver LSP)
-    nodejs nodePackages_latest.typescript-language-server typescript
-    
+    nodejs
+    nodePackages_latest.typescript-language-server
+    typescript
+
     # Lua Runtime (for lua-ls LSP)
-    sumneko-lua-language-server stylua
-    
+    sumneko-lua-language-server
+    stylua
+
     # Haskell Runtime (for haskell-language-server LSP)
-    haskell-language-server hlint ormolu ghc cabal-install stack pandoc
-    
+    haskell-language-server
+    hlint
+    ormolu
+    ghc
+    cabal-install
+    stack
+    pandoc
+
     # Terraform (for terraformls LSP)
-    terraform terraform-ls terraform-docs tflint tfsec
-    
+    terraform
+    terraform-ls
+    terraform-docs
+    tflint
+    tfsec
+
     # Elixir/Erlang Runtime (for elixirls LSP)
-    elixir elixir_ls rebar3 hex
-    
+    elixir
+    elixir_ls
+    rebar3
+    hex
+
     # SQL Databases (for sqlls LSP)
-    sqlite postgresql_15 mysql80 sqlfluff
+    sqlite
+    postgresql_15
+    mysql80
+    sqlfluff
     # Docker & Container Tools (for dockerls/yamlls LSP)
-    docker docker-compose
+    docker
+    docker-compose
     # Kubernetes Tools (for yamlls LSP with k8s schemas)
     kubectl
     # General Development Tools
-    git curl jq yq-go
+    git
+    curl
+    jq
+    yq-go
     # Formatters & Linters that LSPs might use
-    prettier shfmt shellcheck yamllint
+    prettier
+    shfmt
+    shellcheck
+    yamllint
   ];
 
   # Set environment variables for better language support
   extraConfigLua = ''
     -- Python path configuration
     vim.g.python3_host_prog = "${pkgs.python311}/bin/python3"
-    
+
     -- Rust environment
     vim.env.RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}"
-    
+
     -- Elixir environment
     vim.env.MIX_ENV = "dev"
     vim.env.ERL_AFLAGS = "-kernel shell_history enabled"
