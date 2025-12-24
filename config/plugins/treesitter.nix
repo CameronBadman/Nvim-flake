e38fe86dc97b978c8963231acf21dbd90c13a791
Cameron Badman <cbadwork@gmail.com>
2025-12-25 08:37:42 +1000

    fixing treesitter issue

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -5,6 +5,12 @@
     settings = {
       highlight.enable = true;
       indent.enable = true;
+      auto_install = false;
+      sync_install = false;
     };
   };
+
+  extraConfigLua = ''
+    vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/site/parser")
+  '';
 }
