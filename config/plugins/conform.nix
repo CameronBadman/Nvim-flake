7dd929e55a74692c90efbd6168e2db5228d55a2e
Cameron Badman <cbadwork@gmail.com>
2026-01-18 11:26:40 +0000

    fixing formatting issues

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -5,9 +5,15 @@
     settings = {
       # Global format on save behavior
       format_on_save = {
-        timeout_ms = 500;
+        timeout_ms = 3000;
         lsp_fallback = true;
       };
+
+      formatters_by_ft = {
+        "_" = [ "trim_whitespace" ];
+      };
+
+      log_level = "vim.log.levels.DEBUG";
     };
   };
 }
